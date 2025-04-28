0638484a1a1084ccd680e799669522658844f77b
MahdiMac <mahdijj0090@gmail.com>
2025-04-28 10:50:07 +0200

    add homepage, categorycomponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import TestPage from "./pages/test.jsx";
+import LoadingSpinner from "./components/LoadingSpinner.jsx";
 import { Toaster } from "react-hot-toast";
+import { useUserStore } from "./stores/useUserStore.js";
+import { useEffect } from "react";
 function App() {
+  const { user, checkAuth, checkingAuth } = useUserStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (checkingAuth) {return <LoadingSpinner/>};
+
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
       {/* Background gradient */}
@@ -18,15 +29,14 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to="/"/>} />
+          <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/"/>} />
           <Route path="/test" element={<TestPage />} />
         </Routes>
       </div>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 }
 
 export default App;
-
