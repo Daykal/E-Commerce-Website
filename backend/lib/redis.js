e04f95164e6676866418f5bf862f03ad3d8e2e73
MahdiMac <mahdijj0090@gmail.com>
2025-04-28 08:34:03 +0200

    add login, signup pages

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+// export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis("rediss://default:AUdRAAIjcDFhM2Y3MmZjZDdlOTY0MWUzYjE3YjFmMTMyOThmMzRhMnAxMA@together-seasnail-18257.upstash.io:6379");
 // await redis.set('foo', 'bar');
 
